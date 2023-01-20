cd8523719f64cb349f27708cc30d0b5d6b7553d0
KaiYuanee <115628863+KaiYuanee@users.noreply.github.com>
2023-01-20 19:57:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var into_problems_tab = document.getElementsByClassName("into_problems_tab")[0];
+
+into_problems_tab.addEventListener("click", function () {
+    window.location.href = "problems.html";
+});
